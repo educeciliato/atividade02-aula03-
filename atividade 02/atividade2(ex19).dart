b5758dcfc8ca5a123d7e468828adb4187d6de3ce
educeciliato <148019576+educeciliato@users.noreply.github.com>
2025-04-24 20:19:13 -0300

    Update atividade2(ex19).dart

--- atividade 02/atividade2(ex19).dart
+++ atividade 02/atividade2(ex19).dart
@@ -1,3 +1,145 @@
+class Pais {
+  String nome;
+  Pais({required this.nome});
+}
+
+class Estado {
+  String nome;
+  Pais pais;
+  Estado({required this.nome, required this.pais});
+}
+
+class Cidade {
+  String nome;
+  Estado estado;
+  Cidade({required this.nome, required this.estado});
+}
+
+class Endereco {
+  String rua;
+  String cep;
+  Cidade cidade;
+  Endereco({required this.rua, required this.cep, required this.cidade});
+}
+
+class Fornecedor {
+  String nome;
+  String cnpj;
+  Endereco endereco;
+  void Function() acaoFornecedor;
+
+  Fornecedor({
+    required this.nome,
+    required this.cnpj,
+    required this.endereco,
+    required this.acaoFornecedor,
+  });
+}
+
+class TipoProduto {
+  String descricao;
+  void Function() exibirTipo;
+
+  TipoProduto({
+    required this.descricao,
+    required this.exibirTipo,
+  });
+}
+
+class CategoriaProduto {
+  String nome;
+  String descricao;
+  void Function() exibirCategoria;
+
+  CategoriaProduto({
+    required this.nome,
+    required this.descricao,
+    required this.exibirCategoria,
+  });
+}
+
+class UnidadeMedida {
+  String nome;
+  String simbolo;
+  void Function() atualizarUnidade;
+
+  UnidadeMedida({
+    required this.nome,
+    required this.simbolo,
+    required this.atualizarUnidade,
+  });
+}
+
+class Produto {
+  String nome;
+  double preco;
+  TipoProduto tipo;
+  CategoriaProduto categoria;
+  UnidadeMedida unidadeMedida;
+  Fornecedor fornecedor;
+  void Function() ajustarPreco;
+
+  Produto({
+    required this.nome,
+    required this.preco,
+    required this.tipo,
+    required this.categoria,
+    required this.unidadeMedida,
+    required this.fornecedor,
+    required this.ajustarPreco,
+  });
+}
+
+class Cliente {
+  String nome;
+  Endereco endereco;
+  void Function() atualizarEndereco;
+
+  Cliente({
+    required this.nome,
+    required this.endereco,
+    required this.atualizarEndereco,
+  });
+}
+
+class ItemVenda {
+  Produto produto;
+  int quantidade;
+  double precoUnitario;
+
+  ItemVenda({
+    required this.produto,
+    required this.quantidade,
+    required this.precoUnitario,
+  });
+}
+
+class PedidoVenda {
+  Cliente cliente;
+  List<ItemVenda> itens;
+  void Function() concluirVenda;
+
+  PedidoVenda({
+    required this.cliente,
+    required this.itens,
+    required this.concluirVenda,
+  });
+}
+
+class PedidoVendaFreteExpresso {
+  PedidoVenda pedidoVenda;
+  double taxaFreteExpresso;
+  double distancia;
+  void Function() calcularFreteExpresso;
+
+  PedidoVendaFreteExpresso({
+    required this.pedidoVenda,
+    required this.taxaFreteExpresso,
+    required this.distancia,
+    required this.calcularFreteExpresso,
+  });
+}
+
 class ProdutoRestricaoIdade {
   Produto produto;
   int idadeMinima;
@@ -14,26 +156,43 @@ void main() {
   var pais = Pais(nome: "Brasil");
   var estado = Estado(nome: "São Paulo", pais: pais);
   var cidade = Cidade(nome: "São Paulo", estado: estado);
-  var enderecoFornecedor = Endereco(rua: "Rua Fornecedor", cep: "12345-678", cidade: cidade);
+  var enderecoFornecedor = Endereco(
+    rua: "Rua Fornecedor",
+    cep: "12345-678",
+    cidade: cidade,
+  );
 
   var fornecedor = Fornecedor(
-    "Fornecedor de Produtos Restritos",
-    "12.345.678/0001-99",
-    enderecoFornecedor,
+    nome: "Fornecedor de Produtos Restritos",
+    cnpj: "12.345.678/0001-99",
+    endereco: enderecoFornecedor,
     acaoFornecedor: () {
       print("Ação do fornecedor realizada.");
     },
   );
 
   var produto = Produto(
-    "Jogo Eletrônico Violento",
-    199.99,
-    TipoProduto("Entretenimento", exibirTipo: () {}),
-    CategoriaProduto("Jogos", "Jogos eletrônicos para maiores", exibirCategoria: () {}),
+    nome: "Jogo Eletrônico Violento",
+    preco: 199.99,
+    tipo: TipoProduto(
+      descricao: "Entretenimento",
+      exibirTipo: () {
+        print("Tipo de produto: Entretenimento.");
+      },
+    ),
+    categoria: CategoriaProduto(
+      nome: "Jogos",
+      descricao: "Jogos eletrônicos para maiores",
+      exibirCategoria: () {
+        print("Categoria: Jogos eletrônicos para maiores.");
+      },
+    ),
     unidadeMedida: UnidadeMedida(
       nome: "Unidade",
       simbolo: "un",
-      atualizarUnidade: () {},
+      atualizarUnidade: () {
+        print("Unidade de medida atualizada.");
+      },
     ),
     fornecedor: fornecedor,
     ajustarPreco: () {
@@ -48,7 +207,8 @@ void main() {
       if (idadeUsuario >= 18) {
         print("O usuário pode adquirir o produto '${produto.nome}'.");
       } else {
-        print("Restrição de idade! O produto '${produto.nome}' só pode ser adquirido por maiores de 18 anos.");
+        print(
+            "Restrição de idade! O produto '${produto.nome}' só pode ser adquirido por maiores de 18 anos.");
       }
     },
   );
